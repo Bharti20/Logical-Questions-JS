866055c137fa357206787ac0fb339065a5450072
Bharti20 <bhartikumari20@navgurukul.org>
2021-04-08 11:13:48 +0530

    pre and post increament

---
+++ preAndPostIncreament.js
@@ -0,0 +1,11 @@
+//pre-increament: first increament than evaluate.
+var i = 3
+var index = ++i
+console.log(index) //3
+
+//post-increament:first evaluate than increament.
+
+var x = 3
+var y = x++
+console.log(y) //4
+
