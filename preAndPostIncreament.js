dd4989e54c423b6f56f5eabbc3a9c4d4867f5509
Bharti20 <bhartikumari20@navgurukul.org>
2021-04-08 11:20:23 +0530

    add somthing in that

--- preAndPostIncreament.js
+++ preAndPostIncreament.js
@@ -1,11 +1,13 @@
 //pre-increament: first increament than evaluate.
 var i = 3
-var index = ++i
-console.log(index) //3
+var a = ++i
+console.log(i) //4
+console.log(a) //3
 
 //post-increament:first evaluate than increament.
 
 var x = 3
 var y = x++
+console.log(x) //4
 console.log(y) //4
 
